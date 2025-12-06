794a2714941ccc66c67c1d1126ed64d5813d5ae7
joeprice20 <19871383+joeprice20@users.noreply.github.com>
2025-12-06 15:14:34 +0000

    Completed exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    if(num < 0){
+        return "ERROR";
+    }
+    let combinedString = "";
+    for (i = 0; i < num; i++) {
+        combinedString += string;
+        
+    }
+    return combinedString;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
